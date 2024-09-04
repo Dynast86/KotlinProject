6473ad679bd6f087a90aace848e4b3e594f20c69
김창구 <cg.kim@eastarjet.com>
2024-09-04 14:12:42 +0900

    NavigationItem 추가

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,16 +3,15 @@ package org.example.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomAppBar
-import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Button
 import androidx.compose.material.FabPosition
@@ -22,24 +21,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
 import org.example.project.ui.AppViewModel
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
@@ -50,6 +51,7 @@ fun App(
     MaterialTheme {
         val uiState by viewModel.uiState.collectAsStateWithLifecycle()
         val showContent by viewModel.showContent.collectAsStateWithLifecycle()
+        var clicked by remember { mutableStateOf(0) }
 
         Scaffold(
             modifier = Modifier.safeDrawingPadding(),
@@ -60,16 +62,25 @@ fun App(
                 BottomAppBar(
                     modifier = Modifier.height(80.dp)
                         .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)),
-                    contentColor = Color.Gray,
+                    contentColor = Color.White,
+                    backgroundColor = Color.Red,
                     cutoutShape = CircleShape,
                 ) {
-
-//                    Text(
-//                        modifier = Modifier
-//                            .fillMaxWidth(),
-//                        textAlign = TextAlign.Center,
-//                        text = "Bottom app bar",
-//                    )
+                    BottomNavigationItem(
+                        modifier = Modifier.weight(1f),
+                        selected = clicked == 0,
+                        icon = {},
+                        label = { Text("Home", fontSize = 16.sp) },
+                        onClick = { clicked = 0 }
+                    )
+                    Spacer(modifier = Modifier.width(88.dp))
+                    BottomNavigationItem(
+                        modifier = Modifier.weight(1f),
+                        selected = clicked == 2,
+                        icon = {},
+                        label = { Text("More", fontSize = 16.sp) },
+                        onClick = { clicked = 2 }
+                    )
                 }
             },
             floatingActionButtonPosition = FabPosition.Center,
